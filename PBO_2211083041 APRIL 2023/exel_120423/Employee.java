710e397bdd473c787d429bc99092e81b7210f805
exelpratama13 <127178875+exelpratama13@users.noreply.github.com>
2023-08-04 13:53:23 +0700

    Add files via upload

---
+++ PBO_2211083041 APRIL 2023/exel_120423/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exel_120423;
+
+/**
+ *
+ * @author exel
+ */
+public class Employee extends Person {
+    @Override
+    public String getName(){
+        System.out.println("getName Employee");
+        return name;
+    }
+}
